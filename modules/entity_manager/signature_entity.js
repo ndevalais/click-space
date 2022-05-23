2e12c1ce0197060aa2b3e44c122de182606dec73
Nestor de Valais <nestor.devalais@gmail.com>
2022-05-23 11:52:09 -0300

    modifico el tiempo de la firma

--- modules/entity_manager/signature_entity.js
+++ modules/entity_manager/signature_entity.js
@@ -11,10 +11,13 @@ var hash = require('object-hash');
 var request = require('request');
 var log = require("../log");
 
+// Expire the signature horas
+const EXPIRATION_TTL = 24;
 // "Minutos que deben pasar sin consultarse un objeto en la caché antes de olvidarse de el",
-const CACHE_TTK_ELEMENT_NO_ACTIVITY = 240; // 4 horas
+const CACHE_TTK_ELEMENT_NO_ACTIVITY = 60 * EXPIRATION_TTL; // 4 horas
 // "Tiempo de refresco de caché para cada elemento, en segundos.",
-const CACHE_TTL = 14400;
+const CACHE_TTL = 3600 * EXPIRATION_TTL;
+
 
 // Clave de la firma Appsflyer
 const key ='Bearer eyJhbGciOiJBMjU2S1ciLCJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwidHlwIjoiSldUIiwiemlwIjoiREVGIn0.7EgPLtG6tIyZgTQNJ7lV5rAYmopcAZC5tfSGN1Orpz9hd9P-uRTjVw.dMFqbYw1JwbS6oFy.No4E069tos1BN4A09VuEAhFq85rmp4C1TxpIzA9dXF5Ah0uEFoW134oilafS-j1Vw8zSvIhAG5MSZFEW7ItNTvgdUIYhfRWC93My2GeddclzgLXNUd1o2uJJ0ORR9fbRknFqrjSIOLVWIhvZ_P0M2Q6_u8-ysD8q-G4moNpre63ru7IO1QBL3cAVj7yEzNYVwhCO6hQgGmdVa5K6I68F-zoIhIM-jahQQhdCmdnwP8foa8IpNDYq8hVUQh6fkyB2BWuksfhLjTE3hcvk7f0CeyFFvoDbi9IYHc3jqlSuHbypcnKW-Z_r3k_pl5WVqNOe2WRN6ZouhtkVHqBDVkFgt3_a4NjrbeDS-UxD8Udl4AfgO_oN3TL83pHMDvO4me_I158FqZSEmenMPAd7H0dHqbQXzFXq4VBTxi_yejmSWECOv2DUju_DvFabc8ZO1Vn7wtXJKCM4sKCHUDjEZ5-JANXO-OHvs5dKbT9DHk1XtS86P7Ca6aLMuQmJdGy9a61rpIQG7V4rwNNJST9_glM5.osY488-NLBj450rynZnkjQ';
@@ -90,6 +93,8 @@ var saveSignature = async function (signature) {
 
 var signatureAppsflyer = async function () {
     try{ 
+        let temp;
+        let temp1;
         var header =  {
             'Content-Type': 'application/json',
             'Authorization': key
@@ -99,17 +104,21 @@ var signatureAppsflyer = async function () {
             'url': 'https://hq1.appsflyer.com/api/p360-click-signing/secret?ttlHours=4',
             'headers': header
         };
-      var optionsConfig = {
-        'method': 'GET',
-        'url': 'https://hq1.appsflyer.com/api/p360-click-signing/config',
-        'headers': header
-      };
-        //if (!signature["secret-key"]) {
-        let temp = await requestApps(optionsCreate);
+        var optionsConfig = {
+            'method': 'GET',
+            'url': 'https://hq1.appsflyer.com/api/p360-click-signing/config',
+            'headers': header
+        };
+        var optionsEnable = {
+            'method': 'POST',
+            'url': 'https://hq1.appsflyer.com/api/p360-click-signing/config/mode/enabled',
+            'headers': header
+        };
+        temp = await requestApps(optionsCreate);
         // Si no puedo crear nueva firma borro todas las firmas creadas
         if (temp=='Too many active keys') {
           // Elimino las firmas
-          let temp1 = await requestApps(optionsConfig);
+          temp1 = await requestApps(optionsConfig);
           temp1 = JSON.parse(temp1);
           revoca1 = temp1["active-key-ids"][0]["secret-key-id"];
           revoca2 = temp1["active-key-ids"][1][ "secret-key-id"];
@@ -127,6 +136,12 @@ var signatureAppsflyer = async function () {
           await requestApps(optionsDel2);
           // Creo nueva firma
           temp = await requestApps(optionsConfig);
+          // Valido si la firma esta activa
+          if (temp1["mode"]!='enable') await requestApps(optionsEnable);
+        } else {
+            // Valido si la firma esta activa
+            temp1 = await requestApps(optionsConfig);
+            if (temp1["mode"]!='enable') await requestApps(optionsEnable);
         }
         temp = JSON.parse(temp);
         //}
@@ -145,5 +160,5 @@ module.exports = {
 //Get the date + configured days in environment
 function getExpirationDate(){    
     //return new Date(moment().add(config.MONGO_DB_signature_EXPIRATION_IN_DAYS ,'days'));
-    return new Date(moment().add(1, 'h'));
+    return new Date(moment().add(EXPIRATION_TTL, 'h'));
 }
